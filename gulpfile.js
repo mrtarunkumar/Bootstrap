a57291b151997f2ae2b06cb87c25f9cb60c87447
mrtarunkumar <tarunkumar.vegamwala@ntlworld.com>
2018-04-12 07:34:47 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+
+// Compile SASS into CSS, then move files into our /src/css folder & inject into browser
+gulp.task('sass', function(){
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+// Move JS files into our /src/js folder
+gulp.task('js', function(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.js', 'node_modules/jquery/dist/jquery.js', 'node_modules/popper.js/dist/umd/popper.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream());
+});
+
+// Static server & watch for changes in SCSS and HTML files
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({
+        server: './src'
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss', ['sass']]);
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+
+// Gulp default tasks
+gulp.task('default', ['js', 'serve']);
